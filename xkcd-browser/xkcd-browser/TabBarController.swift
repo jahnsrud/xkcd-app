bdd82bec19bbfe15717b82d12acdbae203976f05
Markus Jahnsrud <markus@soriko.no>
2022-03-06 14:32:45 +0100

    Created UIKit Xcode project without storyboard

---
+++ xkcd-browser/xkcd-browser/TabBarController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class TabBarController: UITabBarController {
+    private enum Tab {
+        case latest
+        case browse
+        case favorites
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
