883e29bfeac5e69edeed128a2cccdc7ab28f2120
Markus Jahnsrud <markus@soriko.no>
2022-03-07 21:06:21 +0100

    WIP for supporting fetching of multiple comics for the feed 🔨

--- xkcd-browser/xkcd-browser/View Controllers/Browse/BrowseViewModel.swift
+++ xkcd-browser/xkcd-browser/View Controllers/Browse/BrowseViewModel.swift
@@ -9,15 +9,31 @@ final class BrowseViewModel {
     private var cancellables = Set<AnyCancellable>()
     
     init() {
-        fetchComics()
+        fetchFirstComic()
     }
     
-    private func fetchComics() {
-        let url = URL(string: "https://xkcd.com/info.0.json")!
-        
-        // API:
-        // Get latest: https://xkcd.com/info.0.json
-        // Get specific: https://xkcd.com/-num-/info.0.json
+    // TODO: Move to dedicated service
+    
+    // API Explanation:
+    // Get latest: https://xkcd.com/info.0.json
+    // Get specific: https://xkcd.com/-num-/info.0.json
+    
+    private let latestUrl = URL(string: "https://xkcd.com/info.0.json")!
+    private var newestComicNum: Int?
+    
+    private func fetchFirstComic() {
+        httpClient.fetch(from: latestUrl) { result in
+            switch result {
+            case .success(let data):
+                self.parseFirst(data: data)
+            case .failure(let error):
+                print("Something went wrong: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func fetchComic(number: Int) {
+        let url = URL(string: "https://xkcd.com/\(number)/info.0.json")!
         
         httpClient.fetch(from: url) { result in
             switch result {
@@ -26,6 +42,20 @@ final class BrowseViewModel {
             case .failure(let error):
                 print("Something went wrong: \(error.localizedDescription)")
             }
+        }
+    }
+    
+    private func parseFirst(data: Data) {
+        do {
+            let comics = try JSONDecoder().decode(ComicDTO.self, from: data)
+            self.comicItems.append(comics.toModel())
+            self.newestComicNum = comics.num
+
+            for index in (newestComicNum!-5..<newestComicNum!).reversed() {
+                self.fetchComic(number: index)
+            }
+            
+        } catch {
         }
     }
     
@@ -41,7 +71,7 @@ final class BrowseViewModel {
     
     func didPullToRefresh() {
         comicItems.removeAll()
-        fetchComics()
+        fetchFirstComic()
     }
     
 }
