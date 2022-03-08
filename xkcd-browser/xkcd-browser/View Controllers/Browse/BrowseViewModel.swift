7a267cb957e2f5e01724d75575867c176173a7f9
Markus Jahnsrud <markus@soriko.no>
2022-03-08 19:21:49 +0100

    Moved fetching of comics to XKCDService

--- xkcd-browser/xkcd-browser/View Controllers/Browse/BrowseViewModel.swift
+++ xkcd-browser/xkcd-browser/View Controllers/Browse/BrowseViewModel.swift
@@ -1,77 +1,52 @@
 import Foundation
 import Combine
-import NetworkingKit
 
 final class BrowseViewModel {
-    private let httpClient = HTTPClient()
-
     @Published var comicItems = [Comic]()
     private var cancellables = Set<AnyCancellable>()
+    private let xkcdService = XKCDService()
+    private var newestComicNum: Int?
+    private let numberOfComicsToFetch = 5
     
     init() {
-        fetchFirstComic()
+        loadContent()
     }
     
-    // TODO: Move to dedicated service
-    
-    // API Explanation:
-    // Get latest: https://xkcd.com/info.0.json
-    // Get specific: https://xkcd.com/-num-/info.0.json
-    
-    private let latestUrl = URL(string: "https://xkcd.com/info.0.json")!
-    private var newestComicNum: Int?
-    
-    private func fetchFirstComic() {
-        httpClient.fetch(from: latestUrl) { result in
+    private func loadContent() {
+        xkcdService.fetchFirstComic { result in
             switch result {
-            case .success(let data):
-                self.parseFirst(data: data)
+            case .success(let comic):
+                self.comicItems.append(comic)
+                self.newestComicNum = comic.num
+                self.fetch(numberOfComics: self.numberOfComicsToFetch)
             case .failure(let error):
                 print("Something went wrong: \(error.localizedDescription)")
             }
         }
     }
     
-    private func fetchComic(number: Int) {
-        let url = URL(string: "https://xkcd.com/\(number)/info.0.json")!
-        
-        httpClient.fetch(from: url) { result in
-            switch result {
-            case .success(let data):
-                self.parseResults(data: data)
-            case .failure(let error):
-                print("Something went wrong: \(error.localizedDescription)")
-            }
+    private func fetch(numberOfComics number: Int) {
+        for index in (newestComicNum!-5..<newestComicNum!).reversed() {
+            self.fetchComic(number: index)
         }
     }
-    
-    private func parseFirst(data: Data) {
-        do {
-            let comics = try JSONDecoder().decode(ComicDTO.self, from: data)
-            self.comicItems.append(comics.toModel())
-            self.newestComicNum = comics.num
 
-            for index in (newestComicNum!-5..<newestComicNum!).reversed() {
-                self.fetchComic(number: index)
+    private func fetchComic(number: Int) {
+        xkcdService.fetchComic(number: number) { result in
+            switch result {
+            case .success(let comic):
+                self.comicItems.append(comic)
+            case .failure(let error):
+                print("Something went wrong: \(error.localizedDescription)")
             }
-            
-        } catch {
         }
     }
-    
-    private func parseResults(data: Data) {
-        do {
-            let comics = try JSONDecoder().decode(ComicDTO.self, from: data)
-            self.comicItems.append(comics.toModel())
-        } catch {
-        }
-    }
-    
+
     // MARK: User Actions
     
     func didPullToRefresh() {
         comicItems.removeAll()
-        fetchFirstComic()
+        loadContent()
     }
     
 }
---
+++ xkcd-browser/xkcd-browser/View Controllers/Browse/XKCDService.swift
@@ -0,0 +1,55 @@
+import Foundation
+import NetworkingKit
+
+/// API Explanation:
+/// Get latest: https://xkcd.com/info.0.json
+/// Get specific: https://xkcd.com/-num-/info.0.json
+
+final class XKCDService {
+    private let httpClient = HTTPClient()
+    private let latestUrl = URL(string: "https://xkcd.com/info.0.json")!
+    private var newestComicNum: Int?
+    
+    public func fetchFirstComic(completion: @escaping (Result<Comic, Error>) -> Void) {
+        httpClient.fetch(from: latestUrl) { result in
+            do {
+                switch result {
+                case .success(let data):
+                    let comic = try JSONDecoder().decode(ComicDTO.self, from: data)
+                    completion(.success(comic.toModel()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func fetchComic(number: Int, completion: @escaping (Result<Comic, Error>) -> Void) {
+        let url = URL(string: "https://xkcd.com/\(number)/info.0.json")!
+        
+        httpClient.fetch(from: url) { result in
+            do {
+                switch result {
+                case .success(let data):
+                    let comic = try JSONDecoder().decode(ComicDTO.self, from: data)
+                    completion(.success(comic.toModel()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func parseFirst(data: Data) {
+        do {
+            let comics = try JSONDecoder().decode(ComicDTO.self, from: data)
+            // TODO:
+            // self.comicItems.append(comics.toModel())
+            self.newestComicNum = comics.toModel().num
+        } catch {}
+    }
+}
